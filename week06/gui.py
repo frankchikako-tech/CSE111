948df9c7ac549d5ba92a80e00cd8f133a8e9dcbd
Frank Chika <frankchikako@gmail.com>
2026-02-19 23:47:05 +0100

    .

---
+++ week06/gui.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import Frame, Label, Button
+from number_entry import IntEntry
+import random
+
+def main():
+    root = tk.Tk()
+    frm_main = Frame(root)
+    frm_main.master.title("Dice")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
